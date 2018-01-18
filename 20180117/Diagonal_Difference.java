baad4d3103032da53a040fd2029c99c7cbf99509
Sean Jeon <ameeuk@gmail.com>
2018-01-17 20:25:21 -0800

    Add file

---
+++ 20180117/Diagonal_Difference.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int diagonalDifference(int[][] a, int n) {
+        int sum1=0, sum2=0;
+      //  for(int i = 0; i<n; i++){
+      //      sum1 = sum1 + a[i][i];
+      //  }
+        for(int a_i = 0; a_i < n; a_i++){
+            for(int a_j = 0; a_j < n; a_j++){
+                if(a_i-a_j==0){
+                    sum1 = sum1 + a[a_i][a_j];
+                }
+                if(a_i+a_j==n-1){
+                    sum2 = sum2 + a[a_i][a_j];
+                }
+            }
+        }
+        
+        return Math.abs(sum1-sum2);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] a = new int[n][n];
+        for(int a_i = 0; a_i < n; a_i++){
+            for(int a_j = 0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        int result = diagonalDifference(a,n);
+        System.out.println(result);
+        in.close();
+    }
+}
